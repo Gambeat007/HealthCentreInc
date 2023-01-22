26aa8739f791f0d8b9b0ec7ded2be28b73ae52c8
Łukasz Łaszek <laszek.lukasz@gmail.com>
2023-01-22 16:40:01 +0100

    Initial commit

---
+++ src/main/java/com/github/gambeat007/healthcentreinc/HealthCentreIncApplication.java
@@ -0,0 +1,13 @@
+package com.github.gambeat007.healthcentreinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCentreIncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCentreIncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/gambeat007/healthcentreinc/HealthCentreIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gambeat007.healthcentreinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCentreIncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
